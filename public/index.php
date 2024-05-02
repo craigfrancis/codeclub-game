297f8fab5a650e7863208c376d616318e2870c1c
Craig Francis <craig@craigfrancis.co.uk>
2024-05-02 22:39:15 +0100

    Set territory colours

--- public/index.php
+++ public/index.php
@@ -22,11 +22,80 @@
 
 	$territories[1] = [
 			'name'       => 'Great Britain',
-			'colour'     => 'FF0000',
+			'colour'     => 'F07857',
 			'battalions' => 10,
 			'owner'      => 'Craig',
 			'army'       => 'MyArmy',
 		];
+	$territories[2] = [
+			'name'       => 'Western Europe',
+			'colour'     => 'F287F5',
+			'battalions' => 1234,
+			'owner'      => 'Nicolau',
+			'army'       => 'MyArmy',
+		];
+
+	$territories[3] = [
+			'name'       => 'Southern Europe',
+			'colour'     => '5FF2F6',
+			'battalions' => 987654321,
+			'owner'      => 'Thea',
+			'army'       => 'MyArmy',
+		];
+	$territories[31] = [
+			'name'       => 'Quebec',
+			'colour'     => 'D49137',
+			'battalions' => 314159265,
+			'owner'      => 'Someone',
+			'army'       => 'MyArmy',
+		];
+
+	$territories[20] = [
+			'name'       => 'Egypt',
+			'colour'     => '53BDA5',
+			'battalions' => 7364528901,
+			'owner'      => 'Hatshepsut',
+			'army'       => 'MyArmy',
+		];
+	$territories[25] = [
+			'name'       => 'Madagascar',
+			'colour'     => 'F5C26B',
+			'battalions' => 7364528901,
+			'owner'      => 'Alex',
+			'army'       => 'MyArmy',
+		];
+	$territories[18] = [
+			'name'       => 'Japan',
+			'colour'     => 'BF2C34',
+			'battalions' => 500,
+			'owner'      => 'Japan Person',
+			'army'       => 'MyArmy',
+		];
+
+
+	$territories[11] = [
+			'name'       => 'China',
+			'colour'     => '5C62D6',
+			'battalions' => 500098,
+			'owner'      => 'Ninja',
+			'army'       => 'MyArmy',
+		];
+	$territories[21] = [
+			'name'       => 'North Africa',
+			'colour'     => '3d2b24',
+			'battalions' => 9999999999,
+			'owner'      => 'Kealitile',
+			'army'       => 'MyArmy',
+		];
+	$territories[22] = [
+			'name'       => 'East Africa',
+			'colour'     => '66FF00',
+			'battalions' => 10000000000000000,
+			'owner'      => 'Anna',
+			'army'       => 'MyArmy',
+		];
+
+
 
 ?>
 <!DOCTYPE html>
